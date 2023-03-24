4c2a4f76dd251114737cb9015803a36f60c7b8b6
Roman Borsoev <64916155+borsoy@users.noreply.github.com>
2023-03-24 23:20:16 +0800

    Git init

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array;     
+Console.WriteLine("Введите массив строк через запятую (без пробела)");  
+string elementOfArray = Console.ReadLine();     
+array = elementOfArray.Split(',');              
+
+for (int i = 0; i < array.Length; i++)             
+    Console.Write($"{array[i]}, ");
+
+int count = ArrayCheck(array);                   
+string[] resultArray = FillResultArray(array, count);       
+int ArrayCheck(string[] array)
+{
+    int count = 0;                                
+    for (int i = 0; i < array.Length; i++)        
+    {                                          
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+string[] FillResultArray(string[] array, int count)     
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)                 
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - count] = array[i];
+            count--;                                               
+        }
+    }
+    return result;
+}
+void PrintArray(string[] array)                        
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}");
+}
+
+Console.Write(" ->");           
+PrintArray(resultArray);           
